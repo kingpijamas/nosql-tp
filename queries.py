944899ce3b5cae9bd110c3a1d73030884816b6e2
kpjjpk <juanpablorey.10@gmail.com>
2014-06-10 00:36:42 -0300

    Added statistics

--- queries.py
+++ queries.py
@@ -1,33 +1,52 @@
 from neo4jrestclient.client import GraphDatabase, Node
-from datetime import datetime
+import timeit
 import os
 import re
 
 # where Cypher queries are stored
 QUERY_DIR = 'queries'
+QUERY_RUN_TIMES = 100
+
 # the database
 GDB = GraphDatabase("http://localhost:7474/db/data/")
 
+# auxiliary variables for parsing
+q = returns = None
+# query results go here
+results = None
+
 def parse(file):
+	global q, returns
+
 	matches = re.match('(?P<returns>\(.*\))\s+(?P<q>.*)', file.read())
 	q = matches.group('q')
-	returns = matches.group('returns')
-	return (q, eval(returns))
+	returns = eval(matches.group('returns'))
 
-def run_query(q, returns):
-	before = datetime.now()
+def run_query():
+	global q, returns, results
+
 	results = GDB.query(q=q, returns=returns)
-	delta = datetime.now() - before
-	return (delta, results[0])
+
+def run_query_times(times):
+	global results
+	
+	total_time = timeit.Timer('run_query()', setup="from __main__ import run_query").timeit(times)
+
+	return (total_time, total_time/times, results[0])
 
 def main():
 	for filename in os.listdir(QUERY_DIR):
 		with open(os.path.join(QUERY_DIR, filename), 'r') as file:
 			print("Query:\n\t{filename}".format(filename=filename))
-			q, returns = parse(file)
-			result = run_query(q, returns)
-			print("\nResult:\n\t{result}".format(result=result[1]))
-			print("\nQuery took: {delta}".format(delta=result[0]))
+			
+			parse(file)
+			result = run_query_times(QUERY_RUN_TIMES)
+			
+			print("\nResult:\n\t{result}".format(result=result[2]))
+			print("\nStats:")
+			print("\tRuns: {runs}".format(runs=QUERY_RUN_TIMES))
+			print("\tTotal time: {total_time} seconds".format(total_time=result[0]))
+			print("\tMean query time: {avg_time} seconds".format(avg_time=result[1]))
 
 if __name__ == '__main__':
 	main()
