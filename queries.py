c5706abdc6396ac3921154ba7b1de9866e17b099
kpjjpk <juanpablorey.10@gmail.com>
2014-06-10 22:08:49 -0300

    Adds support for multiline and void-returning queries

--- queries.py
+++ queries.py
@@ -5,7 +5,7 @@ import re
 
 # where Cypher queries are stored
 QUERY_DIR = 'queries'
-QUERY_RUN_TIMES = 100
+QUERY_RUN_TIMES = 1
 
 # the database
 GDB = GraphDatabase("http://localhost:7474/db/data/")
@@ -17,14 +17,13 @@ results = None
 
 def parse(file):
 	global q, returns
-
-	matches = re.match('(?P<returns>\(.*\))\s+(?P<q>.*)', file.read())
+	matches = re.match('(?P<returns>\(.*\))\s+(?P<q>(\s*.*\s*)*)', file.read())
 	q = matches.group('q')
 	returns = eval(matches.group('returns'))
 
 def run_query():
 	global q, returns, results
-
+	
 	results = GDB.query(q=q, returns=returns)
 
 def run_query_times(times):
@@ -32,10 +31,15 @@ def run_query_times(times):
 	
 	total_time = timeit.Timer('run_query()', setup="from __main__ import run_query").timeit(times)
 
-	return (total_time, total_time/times, results[0])
+	if len(results) == 0:
+		results = None
+	else:
+		results = results[0]
+
+	return (total_time, total_time/times, results)
 
 def main():
-	for filename in os.listdir(QUERY_DIR):
+	for filename in sorted(os.listdir(QUERY_DIR)):
 		with open(os.path.join(QUERY_DIR, filename), 'r') as file:
 			print("Query:\n\t{filename}".format(filename=filename))
 			
@@ -46,7 +50,7 @@ def main():
 			print("\nStats:")
 			print("\tRuns: {runs}".format(runs=QUERY_RUN_TIMES))
 			print("\tTotal time: {total_time} seconds".format(total_time=result[0]))
-			print("\tMean query time: {avg_time} seconds".format(avg_time=result[1]))
+			print("\tMean query time: {avg_time} seconds\n\n".format(avg_time=result[1]))
 
 if __name__ == '__main__':
 	main()
